e6a6e6ca5e773aacbbba91874b7c98b2ea1d9742
Lucas Stramke <git_lstramke@icloud.com>
2026-03-23 21:55:02 +0100

    chore(#5): add properties example and logger config file

--- capitly/src/main/java/io/github/infobrees/App.java
+++ capitly/src/main/java/io/github/infobrees/App.java
@@ -1,10 +1,15 @@
 package io.github.infobrees;
 
-/**
- * Hello world!
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
+        logger.info("✅ Starting Capitly...");
         System.out.println("Hello World!");
     }
 }
