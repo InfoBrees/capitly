ffd82773757355740d52cccb2a863b865297a69f
lstramke <167616299+lstramke@users.noreply.github.com>
2026-03-24 19:27:58 +0100

    chore(#5): initialize spring boot maven project and vite svelte frontend (#8)
    * chore(#5): add maven java project with spring boot dependency
    
    * chore(#5): add vite svelte frontend project
    
    * chore(#5): add resources folder
    
    * chore(#5): add properties example and logger config file

---
+++ capitly/src/main/java/io/github/infobrees/App.java
@@ -0,0 +1,15 @@
+package io.github.infobrees;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        logger.info("✅ Starting Capitly...");
+        System.out.println("Hello World!");
+    }
+}
