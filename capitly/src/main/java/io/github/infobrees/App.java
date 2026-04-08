a4bc34e2d9dcebfadd7c3979469f6782728ac581
Toni Nguyen <toni.nguyen@stud.htwk-leipzig.de>
2026-04-08 23:11:00 +0200

    devops(#9): added plugins for backend: Spotless, Spotbug, CheckStyle + added readme + added to pipeline

--- capitly/src/main/java/io/github/infobrees/App.java
+++ capitly/src/main/java/io/github/infobrees/App.java
@@ -2,14 +2,19 @@ package io.github.infobrees;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for Capitly.
+ */
 @SpringBootApplication
 public class App {
 
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
-    public static void main(String[] args) {
-        logger.info("✅ Starting Capitly...");
-        System.out.println("Hello World!");
-    }
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+  public static void main(String[] args) {
+    logger.info("✅ Starting Capitly...");
+    SpringApplication.run(App.class, args);
+  }
 }
