39ee4150f960765fe1f52def1401fddc0b8dbe6d
bartek <98755493+bartekey@users.noreply.github.com>
2026-07-13 21:21:07 +0200

    feat(#39): containerized core finished (#50)
    * feat(#39): containerized core finished
    
    * fix(#39): removed expose
    
    * fix(#39): add dockerignore

--- capitly/core/src/main/java/io/github/infobrees/core/App.java
+++ capitly/core/src/main/java/io/github/infobrees/core/App.java
@@ -1,5 +1,9 @@
 package io.github.infobrees.core;
 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +16,15 @@ public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        logger.info("✅ Starting capitly.core ...");
-        SpringApplication.run(App.class, args);
+        logger.info("✅ Starting capitly.core...");
+        var context = SpringApplication.run(App.class, args);
+
+        var ds = context.getBean(DataSource.class);
+
+        try (Connection c = ds.getConnection()) {
+            System.out.println("DB OK: " + c.getMetaData().getURL());
+        } catch (Exception e) {
+            System.err.println("DB FAILED: " + e.getMessage());
+        }
     }
 }
